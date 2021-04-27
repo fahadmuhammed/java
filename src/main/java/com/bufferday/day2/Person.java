69252e10bd414bbab934c110a2516a48bbd7a067
fahad <fahadmuhammed015@gmail.com>
2021-04-27 22:27:52 +0530

    day3-2

--- src/main/java/com/bufferday/day2/Person.java
+++ src/main/java/com/bufferday/day2/Person.java
@@ -3,10 +3,12 @@ import com.bufferday.day3.Name;
 
 public class Person {
 private Name personName;
+private static int personCounter;
 public Person(Name personName){
     this.personName = personName;
 }
 public Person (){
+    personCounter++;
     /*empty on purpose - default constructor*/
 
 }
@@ -14,6 +16,11 @@ public String helloWorld(){
     return "Hello World";
 }
     public String hello(String name) {
-    return "Hello "+name;
+
+        return "Hello "+name;
+    }
+
+    public static int numberOfPersons() {
+        return personCounter;
     }
 }
--- src/test/java/com/bufferday/day2/PersonTest.java
+++ src/test/java/com/bufferday/day2/PersonTest.java
@@ -16,7 +16,14 @@ public class PersonTest {
   @Test
   public void returnHelloFahad(){
     Person person = new Person();
-    Person person2 = new Person();
     assertEquals("Hello Fahad",person.hello("Fahad"));
+  }
+  @Test
+  public void returnNumberOfPersons(){
+    Person person1 = new Person();
+    Person person2 = new Person();
+    Person person3 = new Person();
+    Person person4 = new Person();
+    assertEquals(4,Person.numberOfPersons());
   }
 }
