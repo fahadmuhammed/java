a82979fdc2434f13fb201a9b9108f1f638e2513d
fahad <fahadmuhammed015@gmail.com>
2021-05-08 18:08:56 +0530

    day4

--- src/main/java/com/bufferday/day2/Person.java
+++ src/main/java/com/bufferday/day2/Person.java
@@ -2,9 +2,13 @@ package com.bufferday.day2;
 import com.bufferday.day3.Name;
 
 public class Person {
-private Name personName;
+    public int age;
+    public boolean meritEligible;
+    public boolean lowIncomeFamily;
+    private Name personName;
 private static int personCounter;
-public Person(Name personName){
+
+    public Person(Name personName){
     this.personName = personName;
 }
 public Person (){
@@ -13,14 +17,26 @@ public Person (){
 
 }
 public String helloWorld(){
-    return "Hello World";
+
+        return "Hello World";
 }
     public String hello(String name) {
-
         return "Hello "+name;
     }
 
     public static int numberOfPersons() {
         return personCounter;
+    }
+
+    public String eligibleToVote() {
+    if (this.age<18){
+        return "No";
+    }
+    else 
+        return "Yes";
+    }
+
+    public boolean meritCumMeans() {
+        return (this.meritEligible && this.lowIncomeFamily);
     }
 }
--- src/test/java/com/bufferday/day2/PersonTest.java
+++ src/test/java/com/bufferday/day2/PersonTest.java
@@ -7,23 +7,80 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PersonTest {
 
   @Test
-  public void returnHelloWorld(){
+  public void returnHelloWorld() {
 
     Person fahad = new Person();
-    assertEquals("Hello World",fahad.helloWorld());
+    assertEquals("Hello World", fahad.helloWorld());
   }
 
   @Test
-  public void returnHelloFahad(){
+  public void returnHelloFahad() {
     Person person = new Person();
-    assertEquals("Hello Fahad",person.hello("Fahad"));
+    assertEquals("Hello Fahad", person.hello("Fahad"));
   }
+
   @Test
-  public void returnNumberOfPersons(){
+  public void returnNumberOfPersons() {
     Person person1 = new Person();
     Person person2 = new Person();
     Person person3 = new Person();
     Person person4 = new Person();
-    assertEquals(4,Person.numberOfPersons());
+    assertEquals(4, Person.numberOfPersons());
+  }
+
+  @Test
+  public void returnNumberOfPersonsInLoop() {
+    Person person1;
+    int i;
+
+    for (i=0; i<4; i++){
+      person1 = new Person();
+    }
+
+    i=0;
+    while (i<4){
+      person1 = new Person();
+      i++;
+    }
+
+      i = 0;
+    do {
+      person1 = new Person();
+      i++;
+    } while (i < 4);
+    assertEquals(12, Person.numberOfPersons());
+  }
+
+  @Test
+  public void demonstrateBoolean() {
+    Person person1 = new Person();
+    person1.age = 20;
+    assertEquals("Yes",person1.eligibleToVote());
+  }
+  @Test
+  public void CheckEligibility(){
+    Person person1 = new Person();
+    person1.meritEligible = false;
+    person1.lowIncomeFamily = true;
+    assertEquals(false,person1.meritCumMeans());
+  }
+  @Test
+  public void demonstrateArrays() {
+    Person persons [] = new Person[4];
+    persons[0] = new Person();
+    persons[1] = new Person();
+    persons[2] = new Person();
+    persons[3] = new Person();
+    for(int i=0; i<persons.length; i++){
+      persons[i] = new Person();
+    }
+    for (Person person : persons) {
+      person.helloWorld();
+    }
+
   }
 }
+
+
+
+
